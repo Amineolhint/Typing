ca997c3ff3d9cfdb5360dfea5fb91d1230514e36
Amineolhint <oulhintmohamadamine71@gmail.com>
2023-06-02 17:15:28 +0200

    add a file index.html and java.js

---
+++ java.js
@@ -0,0 +1,7 @@
+// Setup and start animation! 
+var typed = new Typed('.typed', {
+      strings: ["Mohamed", "Amine", "Oulhit"],
+      typeSpeed: 150,
+      backSpeed: 150,
+      loop: true,
+});
